5b40d349e3715f172bb4ead28505db10602b323c
luizhenrique1725 <araujoluiz1725@gmail.com>
2022-08-24 13:31:40 -0300

    Adicionar arquivos de projeto.

---
+++ CamadaDados/Conexao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamadaDados
+{
+    class Conexao
+    {
+        //public static string Cn = "Data Source = FREITAS; Initial Catalog = dbcomercio; Integrated Security=true";
+        public static string Cn = @"Data Source = CINZASSO\MEUSQLEXPRESS; Initial Catalog = bancosyscomercio; Integrated Security=true";
+    }
+}
